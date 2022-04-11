ca37412f75acf9883f0f6011d09a6968536560f9
juntq <91401178+juntq@users.noreply.github.com>
2022-04-11 16:55:49 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo running script
 test1='1'
 test2='2'
-if [ "test1" = "1" ] && [ no such command ]; then
+if [ expr "${test1}" = "1" ] && [ no such command ]; then
   echo HelloWorld
 else
   echo "Something Wrong"
